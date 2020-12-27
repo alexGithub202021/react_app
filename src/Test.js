c7460c5fc7baeb7b6573b0e0be5a04a05e34110f
alexPc <alex987698@gmail.com>
2020-12-27 05:19:56 +0100

    1t commit

---
+++ src/Test.js
@@ -0,0 +1,11 @@
+function Test() {
+    return (
+        <div className="Test">
+            <header className="Test-header">
+                Hello test 1!
+                </header>
+        </div>
+    );
+}
+
+export default Test;
